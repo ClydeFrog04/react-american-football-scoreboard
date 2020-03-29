53fbc25710fedc8ec38be28de603dbcc97aa0d84
ClydeFrog04 <egan.randal@gmail.com>
2020-03-29 17:13:46 -0600

    quarter button and state complete

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ function App() {
     //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
     const [homeScore, setHomeScore] = useState(0);
     const [awayScore, setAwayScore] = useState(0);
-    const touchdown = team =>{
+    const [currentQuarter, setCurQuarter] = useState(1);
+    //todo: difference between "const setTeamScore = team, score =>" and "function setTeamScore(team, score)"
+    function setTeamScore(team, score){
         if(team === "home"){
-            setHomeScore(homeScore + 7);
+            setHomeScore(homeScore + score);
         }else if(team === "away"){
-            setAwayScore(awayScore + 7);
+            setAwayScore(awayScore + score);
         }
-    };
-    function fieldGoal(team) {//todo: is there a reason to use the method signature like in my touchdown method over a function signature like this one?
-        if(team === "home"){
-            setHomeScore(homeScore + 3);
-        }else if(team === "away"){
-            setAwayScore(awayScore + 3);
-        }
+    }
+    function incrementQuarter(){
+        let newQuarter = ((currentQuarter % 4) + 1);
+        setCurQuarter(newQuarter);
+        console.log(currentQuarter);
     }
 
     return (
@@ -40,17 +40,20 @@ function App() {
                         <div className="away__score">{awayScore}</div>
                     </div>
                 </div>
-                <BottomRow/>
+                <BottomRow quarter={currentQuarter} />
             </section>
             <section className="buttons">
                 <div className="homeButtons">
                     {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-                    <button className="homeButtons__touchdown" onClick={()=>touchdown("home")}>Home Touchdown</button>
-                    <button className="homeButtons__fieldGoal" onClick={()=>fieldGoal("home")}>Home Field Goal</button>
+                    <button className="homeButtons__touchdown" onClick={()=>setTeamScore("home", 7)}>Home Touchdown</button>
+                    <button className="homeButtons__fieldGoal" onClick={()=>setTeamScore("home", 3)}>Home Field Goal</button>
                 </div>
                 <div className="awayButtons">
-                    <button className="awayButtons__touchdown" onClick={()=>touchdown("away")}>Away Touchdown</button>
-                    <button className="awayButtons__fieldGoal" onClick={()=>fieldGoal("away")}>Away Field Goal</button>
+                    <button className="awayButtons__touchdown" onClick={()=>setTeamScore("away", 7)}>Away Touchdown</button>
+                    <button className="awayButtons__fieldGoal" onClick={()=>setTeamScore("away", 3)}>Away Field Goal</button>
+                </div>
+                <div className="gameButtons">
+                    <button className="quarter" onClick={incrementQuarter}>Increment Quarter</button>
                 </div>
             </section>
         </div>
